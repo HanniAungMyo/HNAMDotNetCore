1994e52d53f2a8fce33fccd226c8b112279d2773
HanniAungMyo <hsuhanni095@gmail.com>
2024-09-15 19:17:42 +0630

    feat: ado.net create

--- HNAMDotNetCore.ConsoleApp/Program.cs
+++ HNAMDotNetCore.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using HNAMDotNetCore.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -8,32 +9,7 @@ Console.WriteLine("Hello, World!");
 Console.ReadKey();
 //md =MarkDown
 
-string connectionString = "Data Source=LAPTOP\\SQLSERVER;Initial Catalog=DotNet;User ID=sa; Password=sa@123";
-SqlConnection connection = new SqlConnection(connectionString);
-connection.Open();
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_Blog]";
-SqlCommand cmd = new SqlCommand(query, connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-//DataTable dt=new DataTable();
-//adapter.Fill(dt);
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogId"]);
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-}
-connection.Close();
-//foreach (DataRow dr in dt.Rows)
-//{
-//    Console.WriteLine(dr["BlogId"]);
-//    Console.WriteLine(dr["BlogTitle"]);
-//    Console.WriteLine(dr["BlogAuthor"]);
-//    Console.WriteLine(dr["BlogContent"]);
-//}
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+adoDotNetExample.Read();
+//adoDotNetExample.Create();
+//adoDotNetExample.Delete();
